f83973f80552a9579c6dcbdf300c107e2128e9c0
yef <1061380138@qq.com>
2022-02-23 15:05:06 +0800

    dont sleep,eatting first!

--- src/main/java/com/example/sample/HelloWorld.java
+++ src/main/java/com/example/sample/HelloWorld.java
@@ -4,4 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+    public void eating(){
+        System.out.println("now i wannna eatting food");
+    }
 }
